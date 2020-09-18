8a4180c9cd597497424ad7687831d24564af66d4
steve <steve.liu@holaverse.com>
2020-09-18 10:48:46 +0800

    4006.1

--- TraceAnalysisSDK_4005_1/TraceAnalysisSDK.framework/Headers/TraceAnalysisVersion.h
+++
@@ -1,9 +0,0 @@
-//
-//  TraceAnalysisVersion.h
-//
-//  Created by steve on 2017/3/21.
-//  Copyright © 2017年 steve. All rights reserved.
-//
-
-#define SDKVERSION          @"4005"
-#define SDKVERSIONBUILD     @"1"
