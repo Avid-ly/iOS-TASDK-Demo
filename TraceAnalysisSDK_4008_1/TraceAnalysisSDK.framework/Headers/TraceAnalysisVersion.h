bf8ad328bf3b89fa25891c05aa0d3ab033d717ee
steve <steve.liu@holaverse.com>
2020-11-25 11:19:22 +0800

    Update 4008.1

---
+++ TraceAnalysisSDK_4008_1/TraceAnalysisSDK.framework/Headers/TraceAnalysisVersion.h
@@ -0,0 +1,10 @@
+//
+//  TraceAnalysisVersion.h
+//
+//  Created by steve on 2017/3/21.
+//  Copyright © 2017年 steve. All rights reserved.
+//
+
+#define SDKVERSION          @"4008"
+#define SDKVERSIONBUILD     @"1"
+#define TASDKVersionString  @"TASDK_VERSION_4008.1"
