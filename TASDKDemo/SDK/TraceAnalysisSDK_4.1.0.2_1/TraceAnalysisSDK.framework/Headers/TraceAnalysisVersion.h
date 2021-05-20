81a3d1e6de99f0760fdc084b8f807ff03ca71c13
steve <909476821@qq.com>
2021-05-20 11:02:56 +0800

    4.1.0.2

---
+++ TASDKDemo/SDK/TraceAnalysisSDK_4.1.0.2_1/TraceAnalysisSDK.framework/Headers/TraceAnalysisVersion.h
@@ -0,0 +1,11 @@
+//
+//  TraceAnalysisVersion.h
+//
+//  Created by steve on 2017/3/21.
+//  Copyright © 2017年 steve. All rights reserved.
+//
+
+#define TASDKVERSION        @"4.1.0.2"
+#define SDKVERSION          @"4102"
+#define SDKVERSIONBUILD     @"1"
+#define TASDKVersionString  @"TASDK_VERSION_4102.1"
