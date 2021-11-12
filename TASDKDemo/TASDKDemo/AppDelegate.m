2468b045e40af4aaa87475eb843f046e17cebf4a
steve <909476821@qq.com>
2021-11-12 17:33:50 +0800

    4.1.0.6

--- TASDKDemo/TASDKDemo/AppDelegate.m
+++ TASDKDemo/TASDKDemo/AppDelegate.m
@@ -14,6 +14,7 @@
 #import <AppsFlyerLib/AppsFlyerTracker.h>
 #import <UIKit/UIKit.h>
 #import <AdSupport/AdSupport.h>
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
 
 //#import "SensorsAnalyticsSDK.h"
 
